9609db9ae7f86826f2e34853634a677a1a4bab08
GohanSim <simba-1978@mail.ru>
2026-03-27 21:27:55 +0300

    Create Java2.java

---
+++ Java2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Java2 {
+    public void main(String args[]){
+        Scanner scan = new Scanner(System.in, "cp866");
+        String name = scan.next();
+        System.out.println("Привет " + name + "!\n");            
+    } 
+}
