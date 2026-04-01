62c907c0dd7af8a2de345e908b2484dcd59bfba1
GohanSim <simba-1978@mail.ru>
2026-04-01 16:43:39 +0300

    Create Java4.java

---
+++ Java4.java
@@ -0,0 +1,13 @@
+class TwoWords{
+    public static String Unite(String a, String b){
+        return a + " " + b;
+    }
+}
+
+public class Java4{
+    void main(String[] argc){
+        String a = "Slava";
+        String b = "KPSS";
+        System.out.println(TwoWords.Unite(a, b));
+    }
+}
