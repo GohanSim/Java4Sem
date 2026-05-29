48aa19de557fc8e527ed1f3d784a1426edd9e3b2
GohanSim <simba-1978@mail.ru>
2026-05-29 11:28:02 +0300

    Update Java16

---
+++ Java16.java
@@ -0,0 +1,40 @@
+class A {
+    public void printNum(Integer i) {
+        System.out.println("Integer = " + i);
+    }
+    public void printNum(int i) {
+        System.out.println("int = " + i);
+    }
+    public void printNum(Float f) {
+        System.out.println("Float = " + f);
+    }
+    public void printNum(Number n) {
+        System.out.println("Number = " + n);
+    }
+}
+
+public class Java16 {
+    public static void main(String[] args) {
+        A a = new A();
+        Number[] num = {
+                Integer.valueOf(1),
+                11,
+                1.11f,
+                11.11
+        };
+        // цикл for
+        for (Number n : num)
+            a.printNum(n);
+        System.out.println();
+        // прямые вызовы
+        a.printNum(Integer.valueOf(1));
+        a.printNum(11);
+        a.printNum(1.11f);
+        a.printNum(11.11);
+        /*результат отличается потому что:
+        в цикле n имеет тип Number
+        поэтому вызывается printNum(Number)
+        при прямых вызовах компилятор
+        выбирает наиболее подходящий метод*/
+    }
+}
