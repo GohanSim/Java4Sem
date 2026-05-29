c0ff9f960d6d73887b2572859859aebd484614fc
GohanSim <simba-1978@mail.ru>
2026-05-30 00:05:17 +0300

    Create Java23.java

---
+++ Java23.java
@@ -0,0 +1,98 @@
+// практика 1 перегрузка методов
+
+class Calculator23 {
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    double sum(double a, double b) {
+        return a + b;
+    }
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+// практика 2 переопределение методов
+
+class Parent23 {
+
+    void print() {
+        System.out.println("метод Parent23");
+    }
+}
+
+class Child23 extends Parent23 {
+
+    @Override
+    void print() {
+        System.out.println("метод Child23");
+    }
+
+    /*
+    ошибка:
+
+    нельзя изменить тип возвращаемого значения,
+    если сигнатура метода одинаковая
+
+    пример ошибки:
+
+    int print() {
+        return 1;
+    }
+    */
+}
+
+// практика 3 @Override
+
+class Animal23 {
+
+    void sound() {
+        System.out.println("звук");
+    }
+}
+
+class Dog23 extends Animal23 {
+
+    @Override
+    void sound() {
+        System.out.println("гав");
+    }
+
+    /*
+    если написать так:
+
+    void sounds()
+
+    то это уже будет новый метод,
+    а не переопределение
+
+    @Override сразу покажет ошибку
+    компиляции и поможет найти опечатку
+    */
+}
+
+public class Java23 {
+
+    public static void main(String[] args) {
+
+        // перегрузка
+        Calculator23 calc = new Calculator23();
+
+        System.out.println(calc.sum(2, 3));
+        System.out.println(calc.sum(2.5, 3.5));
+        System.out.println(calc.sum(1, 2, 3));
+
+        // переопределение
+        Parent23 obj = new Child23();
+
+        obj.print();
+
+        // override
+        Dog23 dog = new Dog23();
+
+        dog.sound();
+    }
+}
