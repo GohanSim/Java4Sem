453ac82195302e1d1e7804ca58ab6b977f63e2d1
GohanSim <simba-1978@mail.ru>
2026-05-30 10:45:13 +0300

    Create Java28.java

---
+++ Java28.java
@@ -0,0 +1,73 @@
+// практика 1 default access interface
+// доступен только внутри пакета
+
+interface Interface28A {
+
+    void methodA();
+}
+
+// практика 2 public interface
+// доступен отовсюду
+
+public class Java28 {
+
+    public static void main(String[] args) {
+
+        Test28 obj = new Test28();
+
+        obj.methodA();
+        obj.methodB();
+    }
+}
+
+interface Interface28B {
+    // поля автоматически:
+    // public static final
+    int VALUE = 100;
+    // методы автоматически:
+    // public abstract
+    void methodB();
+}
+
+// класс реализует интерфейсы
+
+class Test28 implements Interface28A, Interface28B {
+
+    @Override
+    public void methodA() {
+        System.out.println("methodA");
+    }
+    @Override
+    public void methodB() {
+        System.out.println("methodB");
+    }
+}
+
+// практика 3 protected interface
+
+/*protected interface нельзя объявить
+на верхнем уровне файла
+protected можно использовать
+только для вложенных интерфейсов*/
+
+class Outer28 {
+
+    protected interface Protected28 {
+
+        void print();
+    }
+}
+
+// практика 4 private interface
+
+/*private interface нельзя объявить
+на верхнем уровне файла
+private интерфейс возможен
+только как вложенный interface*/
+
+class OuterPrivate28 {
+
+    private interface Private28 {
+        void show();
+    }
+}
