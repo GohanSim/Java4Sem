3a2108284df563f681e50c15f4eaec0a53d9e3dc
GohanSim <simba-1978@mail.ru>
2026-05-30 11:05:52 +0300

    Create Java38.java

---
+++ Java38.java
@@ -0,0 +1,38 @@
+public class Java38 {
+
+    public static void main(String[] args) {
+
+        // практика 1
+        /*использовать try без catch
+        и finally нельзя
+        такой код не скомпилируется
+        try {
+            int a = 10;
+        }
+        */
+        // практика 2
+        /*try только с finally
+        использовать можно*/
+
+        try {
+            int a = 10 / 2;
+            System.out.println(a);
+        } finally {
+            System.out.println(
+                    "finally выполнится всегда"
+            );
+        }
+
+        // практика 3
+        /*два finally использовать нельзя
+        такой код не скомпилируется
+        try {
+            int a = 10;
+        } finally {
+            System.out.println(1);
+        } finally {
+            System.out.println(2);
+        }
+        */
+    }
+}
